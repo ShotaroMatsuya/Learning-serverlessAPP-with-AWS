b85e072f68ce64e5238440880ce42b7055a98fea
ShotaroMatsuya <matsuya@h01.itscom.net>
2021-02-28 06:30:03 +0900

    implement e-mail signUp by cognito

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -5,13 +5,36 @@ import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
+// import 'cross-fetch/polyfill';
+// import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
+
+import {
+	CognitoUserPool,
+	CognitoUserAttribute,
+	CognitoUser,
+} from 'amazon-cognito-identity-js';
+
 import { User } from './user.model';
+
+import {AWS_CONFIG} from '../../config/config.secret';
+
+//defined config about aws cognito
+const POOL_DATA = {
+  UserPoolId:AWS_CONFIG.Pool,
+  ClientId:AWS_CONFIG.Client
+};
+
+//create our pool object
+const userPool = new CognitoUserPool(POOL_DATA);
 
 @Injectable()
 export class AuthService {
   authIsLoading = new BehaviorSubject<boolean>(false);
   authDidFail = new BehaviorSubject<boolean>(false);
   authStatusChanged = new Subject<boolean>();
+  //add property about fetched user info form cognito
+  registeredUser :CognitoUser;
+
   constructor(private router: Router) {}
   signUp(username: string, email: string, password: string): void {
     this.authIsLoading.next(true);
@@ -20,10 +43,28 @@ export class AuthService {
       email: email,
       password: password
     };
+    //empty array of attrList
+    const attrList: CognitoUserAttribute[] = [];
+    //create email attribute
     const emailAttribute = {
       Name: 'email',
       Value: user.email
     };
+    //pushing them to the list
+    attrList.push(new CognitoUserAttribute(emailAttribute));
+    //call up signUp method on the userPool object
+    userPool.signUp(user.username,user.password,attrList,null,(err,result)=>{
+      if(err){
+        this.authDidFail.next(true);
+        this.authIsLoading.next(false);
+        console.log(err);
+        return;
+      }
+      this.authDidFail.next(false);
+      this.authIsLoading.next(false);
+      //add populated data to property if it was successful
+      this.registeredUser = result.user;
+    });
     return;
   }
   confirmUser(username: string, code: string) {
